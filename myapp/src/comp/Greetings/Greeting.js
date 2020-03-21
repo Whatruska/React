266ee517502d29acf348e7f29d28067f3c9e4570
Whatruska <vitdub@inbox.ru>
2020-03-21 21:29:41 +0600

    greetings

--- myapp/src/comp/Greetings/Greeting.js
+++ myapp/src/comp/Greetings/Greeting.js
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./Greeting.module.css";
+import gif from "../../img/Greetings.gif"
 
 let Greetings = () => {
     return(
         <div className={classes.Greeting}>
-            <h2 className={classes.title}>Greetings!</h2>
+            <img src={gif} alt="gif"/>
         </div>
     );
 };
