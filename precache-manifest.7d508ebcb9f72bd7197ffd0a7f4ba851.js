8e47dc698176d30c62dc5f19fcedc595c6616714
Whatruska <vitdub@inbox.ru>
2020-04-01 15:04:11 +0600

    Updates

---
+++ precache-manifest.7d508ebcb9f72bd7197ffd0a7f4ba851.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16754e2e66f0b73b8b3f2b8ee200773",
+    "url": "/React-deploy/index.html"
+  },
+  {
+    "revision": "65f699913f1c02c72c9c",
+    "url": "/React-deploy/static/css/main.526a7d6c.chunk.css"
+  },
+  {
+    "revision": "3f8850044eae14fe53bf",
+    "url": "/React-deploy/static/js/2.38fc322d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-deploy/static/js/2.38fc322d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65f699913f1c02c72c9c",
+    "url": "/React-deploy/static/js/main.026f3cb4.chunk.js"
+  },
+  {
+    "revision": "eb15d17f90c23effd1dd",
+    "url": "/React-deploy/static/js/runtime-main.f09f5312.js"
+  },
+  {
+    "revision": "9c5e0711b6d55b03f3df2b7adf484ab8",
+    "url": "/React-deploy/static/media/Anya.9c5e0711.jpg"
+  },
+  {
+    "revision": "d301fdc8f718cc4e956c6456eb2af1ee",
+    "url": "/React-deploy/static/media/Greetings.d301fdc8.gif"
+  },
+  {
+    "revision": "7547549a543f6cfbde9f4951649e896d",
+    "url": "/React-deploy/static/media/Misha.7547549a.jpg"
+  },
+  {
+    "revision": "a704a6aef3e42c2a3e1e5022ae0e19f0",
+    "url": "/React-deploy/static/media/Zeka.a704a6ae.jpg"
+  },
+  {
+    "revision": "65808d6a6bf0dbc426056e4840bd82b8",
+    "url": "/React-deploy/static/media/ava.65808d6a.jpeg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-deploy/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b1df8c1d91d7868f1fae88833cb16aa9",
+    "url": "/React-deploy/static/media/preloader.b1df8c1d.svg"
+  },
+  {
+    "revision": "d05892b6f8a2a81a97dbce5c6eff9437",
+    "url": "/React-deploy/static/media/profile-header.d05892b6.jpeg"
+  },
+  {
+    "revision": "0f16ef6a509cceb0df1037e14780342c",
+    "url": "/React-deploy/static/media/user.0f16ef6a.png"
+  }
+]);
